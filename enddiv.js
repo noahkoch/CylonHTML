8cb789f12cec4c7187e785ca6a3d983ed6bb4a81
Noah Koch <noahtkoch@gmail.com>
2013-11-08 16:36:23 -0600

    Add support for variables and HTML attributes

--- enddiv.js
+++ enddiv.js
@@ -4,45 +4,61 @@ var EndDiv = {
 	
 	initialize: function(){
 		EndDiv.parse();
-		EndDiv.variables();
 	},
 
 	parse: function(){
-
 		var nonParsedCode = $('body').html();
-		var parsedCode = nonParsedCode;
 
+		var parsedCode = EndDiv.addHTMLAttributes(nonParsedCode);
+		parsedCode = EndDiv.findVariables(parsedCode);	
+		parsedCode = EndDiv.findAndReplaceVariables(parsedCode);
+
+		$('body').html(parsedCode);
+	},
+
+	findVariables: function(parsedCode){
+		window.variableHash = {}
+		$.each(parsedCode.match(/`(.+?)`/g),function(ind,declaration){
+
+			// Match array returns as [original,variable,value]
+			var seperate = declaration.match(RegExp(/`(.+?)=(.+?)`/));
+			// Remove the existance of this variable from the document
+			parsedCode = parsedCode.replace(seperate[0],'');
+			variableHash[seperate[1].trim()] = seperate[2].trim();
+		});
+		return parsedCode;
+	},
+
+	addHTMLAttributes: function(nonParsedCode){
+		
+		var parsedCode = nonParsedCode;
 		// Items will be replaced in order 
 		// output is the key, input is the value in regex format
 		characterMap = {
+			// gets all set attributes
+			'' : /[(](.+?)[\/]/g,
 			// Add character clauses below
-			'' : /[(](.+?)[\/]/,
+			// 
 			// Div clauses must be last as it is the default
 			'<div>'	  :    /\(/g,
 			'</div>'	:    /\)/g
 		}
 
 		$.each(characterMap, function(replaceKey,findKey){
-			// '<' + /[(](.+?)[\/]/ + '>' /
-			if(replaceKey == ''){
-				parsedCode = parsedCode.replace(findKey,'<$1>')
-			}
-			else{
-				parsedCode = parsedCode.replace(findKey,replaceKey)
-			}
+			parsedCode = (replaceKey == '') ? parsedCode.replace(findKey,'<$1>') : parsedCode.replace(findKey,replaceKey);
 		})
 		
-		console.log(parsedCode);
-		$('body').html(parsedCode)
+		console.log('non browser interpreted code \n' + parsedCode);
+		return parsedCode;
 
 	},
 
-	variables: function(){
-		variableHash = {}
-	},
-
-	add_to_variables: function(variable,contents){
-
+	findAndReplaceVariables: function(parsedCode){
+		$.each(variableHash, function(variable,value){
+			console.log(new RegExp('`' + variable));
+			parsedCode = parsedCode.replace(new RegExp('`' + variable),value);
+		})
+		return parsedCode;
 	}
 
 };
